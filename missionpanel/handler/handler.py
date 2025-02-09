c11aed14e38e0df771e344277c274cc644c94edc
yindaheng98 <yindaheng98@gmail.com>
2025-02-09 22:58:55 +0800

    Interface mode

--- missionpanel/handler/handler.py
+++ missionpanel/handler/handler.py
@@ -6,12 +6,7 @@ from missionpanel.orm import Mission, Tag, MissionTag, Attempt
 from sqlalchemy import func, distinct, case
 
 
-class Handler(abc.ABC):
-    def __init__(self, session: Session, name: str, max_time_interval: datetime.timedelta = datetime.timedelta(seconds=1)):
-        self.session = session
-        self.name = name
-        self.max_time_interval = max_time_interval
-
+class HandlerInterface(abc.ABC):
     @abc.abstractmethod
     def select_mission(self, missions: Query[Mission]) -> Optional[Mission]:
         return missions[0] if missions else None
@@ -20,9 +15,10 @@ class Handler(abc.ABC):
     def execute_mission(self, mission: Mission, attempt: Attempt) -> bool:
         pass
 
-    def query_missions_by_tag(self, tags: List[str]) -> Query[Mission]:
+    @staticmethod
+    def query_missions_by_tag(session: Session, tags: List[str]) -> Query[Mission]:
         return (
-            self.session.query(Mission)
+            session.query(Mission)
             .join(MissionTag)
             .join(Tag)
             .filter(Tag.name.in_(tags))
@@ -30,9 +26,10 @@ class Handler(abc.ABC):
             .having(func.count(distinct(Tag.name)) == len(tags))
         )
 
-    def query_todo_missions(self, tags: List[str]) -> Query[Mission]:
+    @staticmethod
+    def query_todo_missions(session: Session, tags: List[str]) -> Query[Mission]:
         return (
-            self.query_missions_by_tag(tags)
+            HandlerInterface.query_missions_by_tag(session, tags)
             .outerjoin(Attempt)
             .group_by(Mission.id)
             .having(func.count(
@@ -42,18 +39,34 @@ class Handler(abc.ABC):
                 ), 0), else_=None)) <= 0)  # get those Missions that have no finished or working Attempt
         )
 
-    def run_once(self, tags: List[str]):
-        missions = self.query_todo_missions(tags).all()
+    def run_once(self, session: Session, tags: List[str], name: str, max_time_interval: datetime.timedelta = datetime.timedelta(seconds=1)):
+        missions = HandlerInterface.query_todo_missions(session, tags).all()
         mission = self.select_mission(missions)
         if mission is None:
             return
         attempt = Attempt(
-            handler=self.name,
-            max_time_interval=self.max_time_interval,
+            handler=name,
+            max_time_interval=max_time_interval,
             content=mission.content,
             mission=mission)
-        self.session.add(attempt)
-        self.session.commit()
+        session.add(attempt)
+        session.commit()
         if self.execute_mission(mission, attempt):
             attempt.success = True
-            self.session.commit()
+            session.commit()
+
+
+class Handler(HandlerInterface):
+    def __init__(self, session: Session, name: str, max_time_interval: datetime.timedelta = datetime.timedelta(seconds=1)):
+        self.session = session
+        self.name = name
+        self.max_time_interval = max_time_interval
+
+    def query_missions_by_tag(self, tags: List[str]) -> Query[Mission]:
+        return HandlerInterface.query_missions_by_tag(self.session, tags)
+
+    def query_todo_missions(self, tags: List[str]) -> Query[Mission]:
+        return HandlerInterface.query_todo_missions(self.session, tags)
+
+    def run_once(self, tags: List[str]):
+        super().run_once(self.session, tags, self.name, self.max_time_interval)
--- missionpanel/submitter/submitter.py
+++ missionpanel/submitter/submitter.py
@@ -1,39 +1,55 @@
+import abc
 from typing import List
 from sqlalchemy.orm import Session
 from missionpanel.orm import Mission, Tag, Matcher, MissionTag
 
 
-class Submitter:
-    def __init__(self, session: Session):
-        self.session = session
-
-    def match_mission(self, match_patterns: List[str]) -> Mission:
-        matcher = self.session.query(Matcher).filter(Matcher.pattern.in_(match_patterns)).with_for_update().first()
+class SubmitterInterface(abc.ABC):
+    @staticmethod
+    def match_mission(session: Session, match_patterns: List[str]) -> Mission:
+        matcher = session.query(Matcher).filter(Matcher.pattern.in_(match_patterns)).with_for_update().first()
         if matcher:
             exist_patterns = [matcher.pattern for matcher in matcher.mission.matchers]
-            self.session.add_all([Matcher(pattern=pattern, mission=matcher.mission) for pattern in match_patterns if pattern not in exist_patterns])
-            self.session.commit()
+            session.add_all([Matcher(pattern=pattern, mission=matcher.mission) for pattern in match_patterns if pattern not in exist_patterns])
+            session.commit()
             return matcher.mission
 
-    def create_mission(self, content: str, match_patterns: List[str]):
-        mission = self.match_mission(match_patterns)
+    @staticmethod
+    def create_mission(session: Session, content: str, match_patterns: List[str]):
+        mission = SubmitterInterface.match_mission(session, match_patterns)
         if mission is None:
-            self.session.add(Mission(
+            session.add(Mission(
                 content=content,
                 matchers=[Matcher(pattern=pattern) for pattern in match_patterns],
             ))
         else:
             if mission.content != content:
                 mission.content = content
-        self.session.commit()
+        session.commit()
 
-    def _add_tags(self, mission: Mission, tags_name: List[str]):
-        exist_tags = self.session.query(Tag).filter(Tag.name.in_(tags_name)).with_for_update().all()
+    @staticmethod
+    def _add_tags(session: Session, mission: Mission, tags_name: List[str]):
+        exist_tags = session.query(Tag).filter(Tag.name.in_(tags_name)).with_for_update().all()
         exist_tags_name = [tag.name for tag in exist_tags]
-        self.session.add_all([Tag(name=tag_name) for tag_name in tags_name if tag_name not in exist_tags_name])
+        session.add_all([Tag(name=tag_name) for tag_name in tags_name if tag_name not in exist_tags_name])
         exist_mission_tags = {mission_tag.tag_name: mission_tag.tag for mission_tag in mission.tags}
-        self.session.add_all([MissionTag(mission=mission, tag_name=tag_name) for tag_name in tags_name if tag_name not in exist_mission_tags])
-        self.session.commit()
+        session.add_all([MissionTag(mission=mission, tag_name=tag_name) for tag_name in tags_name if tag_name not in exist_mission_tags])
+        session.commit()
+
+    @staticmethod
+    def add_tags(session: Session, matchers: List[str], tags: List[str]):
+        SubmitterInterface._add_tags(session, SubmitterInterface.match_mission(session, matchers), tags)
+
+
+class Submitter(SubmitterInterface):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def match_mission(self, match_patterns: List[str]) -> Mission:
+        return SubmitterInterface.match_mission(self.session, match_patterns)
+
+    def create_mission(self, content: str, match_patterns: List[str]):
+        return SubmitterInterface.create_mission(self.session, content, match_patterns)
 
     def add_tags(self, matchers: List[str], tags: List[str]):
-        self._add_tags(self.match_mission(matchers), tags)
+        return SubmitterInterface.add_tags(self.session, matchers, tags)
