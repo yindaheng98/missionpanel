4e98808e3e977cae2bfb44ab4050b29ba6d6075c
yindaheng98 <yindaheng98@gmail.com>
2025-03-06 11:55:00 -0800

    catch error in rsshub submit

--- missionpanel/example/rsshub.py
+++ missionpanel/example/rsshub.py
@@ -1,11 +1,13 @@
 import abc
 from typing import AsyncGenerator, List, Any
 import httpx
+import logging
 from xml.etree import ElementTree
 from missionpanel.submitter import AsyncSubmitter
 
 
 class RSSHubSubmitter(AsyncSubmitter, metaclass=abc.ABCMeta):
+    logger = logging.getLogger("RSSHubSubmitter")
 
     @abc.abstractmethod
     async def parse_xml(self, xml: str) -> AsyncGenerator[dict, Any]:
@@ -21,11 +23,14 @@ class RSSHubSubmitter(AsyncSubmitter, metaclass=abc.ABCMeta):
         async with httpx.AsyncClient(**httpx_client_options) as client:
             response = await client.get(rsshub)
             async for mission_content in self.parse_xml(response.text):
-                matchers = await self.derive_matcher(mission_content)
-                await self.create_mission(mission_content, matchers)
-                tags = await self.derive_tags(mission_content)
-                if len(tags) > 0:
-                    await self.add_tags(matchers, tags)
+                try:
+                    matchers = await self.derive_matcher(mission_content)
+                    await self.create_mission(mission_content, matchers)
+                    tags = await self.derive_tags(mission_content)
+                    if len(tags) > 0:
+                        await self.add_tags(matchers, tags)
+                except Exception as e:
+                    self.logger.warning(f'create mission failed, error: {e}')
 
 
 class RSSHubRootSubmitter(RSSHubSubmitter):
