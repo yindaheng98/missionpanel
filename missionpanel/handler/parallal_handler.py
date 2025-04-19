c3252728c14a171870028527d65d0d881098461f
yindaheng98 <yindaheng98@gmail.com>
2025-04-19 15:06:33 -0700

    RuntimeError: dictionary changed size during iteration

--- missionpanel/handler/parallal_handler.py
+++ missionpanel/handler/parallal_handler.py
@@ -34,5 +34,4 @@ class ParallelAsyncHandler(AsyncHandler, abc.ABC):
                 self.task_dict[id] = asyncio.create_task(task(mission, attempt, id))
         for i in self.task_dict:
             await self.task_dict[i]
-        for i in self.task_dict:
-            del self.task_dict[i]
+        self.task_dict = {}
